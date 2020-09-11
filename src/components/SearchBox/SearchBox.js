fd55779a6e22ee8267185768767b3d6e42e15176
Ivan Afonchenko <afon.himself@gmail.com>
2020-09-11 12:55:59 +0300

    add search box

---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './SearchBox.css';
+
+class SearchBox extends Component {
+    state = {
+        searchLine: ''
+    }
+    searchLineChangeHandler = (e) => {
+        this.setState({ searchLine: e.target.value });
+    }
+    searchBoxSubmitHandler = (e) => {
+        e.preventDefault();
+    }
+    render() {
+        const { searchLine } = this.state;
+
+        return (
+            <div className="search-box">
+                <form className="search-box__form" onSubmit={this.searchBoxSubmitHandler}>
+                    <label className="search-box__form-label">
+                        Искать фильм по названию:
+                        <input
+                            value={searchLine}
+                            type="text"
+                            className="search-box__form-input"
+                            placeholder="Например, Shawshank Redemption"
+                            onChange={this.searchLineChangeHandler}
+                        />
+                    </label>
+                    <button
+                        type="submit"
+                        className="search-box__form-submit"
+                        disabled={!searchLine}
+                    >
+                        Искать
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+ 
+export default SearchBox;
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './MainPage.css';
 import Header from '../../components/Header/Header';
+import SearchBox from '../../components/SearchBox/SearchBox';
 
 class MainPage extends Component {
     render() { 
         return (
             <div className="main-page">
                 <Header />
+                <main className="main-page__content">
+                    <section className="main-page__main-section">
+                        <div className="main-page__search-box">
+                            <SearchBox />
+                        </div>
+                        <div className="main-page__movies" />
+                    </section>
+                    <aside className="main-page__favorites">
+                    </aside>
+                </main>
             </div>
         );
     }
