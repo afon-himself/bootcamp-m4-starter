7172d6e548f16c07e6271dba5e76d25f5afe5390
Ivan Afonchenko <afon.himself@gmail.com>
2020-09-11 11:49:27 +0300

    add header

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './MainPage.css';
+import Header from '../../components/Header/Header';
 
 class MainPage extends Component {
     render() { 
         return (
             <div className="main-page">
-                <h1>Main Page</h1>
+                <Header />
             </div>
         );
     }
