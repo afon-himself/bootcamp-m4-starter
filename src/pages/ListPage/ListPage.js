878b962dc76973f82b379c562902d74ee26514d4
Ivan Afonchenko <afon.himself@gmail.com>
2020-09-14 17:42:55 +0300

    add list page

--- src/pages/ListPage/ListPage.js
+++ src/pages/ListPage/ListPage.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import './ListPage.css';
 
 class ListPage extends Component {
+    state = {
+        movies: [
+            { title: 'The Godfather', year: 1972, imdbID: 'tt0068646' }
+        ]
+    }
+    componentDidMount() {
+        const id = this.props.match.params;
+        console.log(id);
+        // TODO: запрос к сервер на получение списка
+        // TODO: запросы к серверу по всем imdbID
+    }
     render() { 
         return (
             <div className="list-page">
-                <h1>List Page</h1>
+                <h1 className="list-page__title">Мой список</h1>
+                <ul>
+                    {this.state.movies.map((item) => {
+                        return (
+                            <li key={item.imdbID}>
+                                <a href="https://www.imdb.com/title/tt0068646/" target="_blank">{item.title} ({item.year})</a>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         );
     }
