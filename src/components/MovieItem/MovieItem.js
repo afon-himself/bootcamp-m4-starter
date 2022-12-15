32aff9351fa86e4d0e3a590479aa7214a8b05011
XXVIIHR <hakimzade.27@gmail.com>
2022-12-15 10:43:09 +0400

    Next step

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,19 +1,55 @@
-import React, { Component } from 'react';
-import './MovieItem.css';
+import React, { Component } from "react";
+import "./MovieItem.css";
+import { addFavoriteList } from "../../redux/actions";
+import { connect } from "react-redux";
 
 class MovieItem extends Component {
-    render() {
-        const { title, year, poster } = this.props;
-        return (
-            <article className="movie-item">
-                <img className="movie-item__poster" src={poster} alt={title} />
-                <div className="movie-item__info">
-                    <h3 className="movie-item__title">{title}&nbsp;({year})</h3>
-                    <button type="button" className="movie-item__add-button">Добавить в список</button>
-                </div>
-            </article>
-        );
-    }
+    ifIdInFavorites = (imdbID) => {
+        const active = this.props.favoriteList.find((item) => {
+            return item.imdbID === imdbID;
+    });
+        if (active) {
+            return true;
+        }
+    };
+
+  render() {
+    const { Title, Year, Poster, imdbID } = this.props;
+    return (
+        <article className="movie-item">
+            <img className="movie-item__poster" src={Poster} alt={Title} />
+            <div className="movie-item__info">
+                <h3 className="movie-item__title">
+                {Title} ({Year})
+                </h3>
+                <button
+                type="button"
+                className="movie-item__add-button"
+                onClick={() => {
+                    this.props.addFavoriteList(imdbID);
+                }}
+                disabled={this.ifIdInFavorites(imdbID)}
+                >
+                {this.ifIdInFavorites(imdbID) ? `✓ Добавлено` : "Добавить в список"}
+                </button>
+            </div>
+        </article>
+    );
+  }
 }
- 
-export default MovieItem;
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addFavoriteList: (imdbID) => {
+            dispatch(addFavoriteList(imdbID));
+        },
+    };
+};
+
+const mapStateToProps = (state) => {
+    return {
+        favoriteList: state.favoriteList,
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieItem);
